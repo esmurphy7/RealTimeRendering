f1dba690dd69fefb7ec037b02b806b12d15c81c3
esmurphy7 <esmurphy7@gmail.com>
2016-07-12 18:26:55 -0700

    generate random heights (no perlin noise)

--- Assignment3/TerrainMesh.h
+++ Assignment3/TerrainMesh.h
@@ -5,6 +5,7 @@ class TerrainMesh
 private:
 	int TERRAIN_X, TERRAIN_Z;
 	float Y_POSITION;	
+	float generateHeight(glm::vec3 point, float H, float lacunarity, int octaves);
 
 public:
 	std::vector<float> vertices;
@@ -22,7 +23,6 @@ TerrainMesh::TerrainMesh(int terrainX, int terrainZ, float yPos)
 	Y_POSITION = yPos;
 	vertices = std::vector<float>();
 	indices = std::vector<unsigned int>();
-	heights = std::vector<float>();
 }
 
 void TerrainMesh::generate()
@@ -45,8 +45,12 @@ void TerrainMesh::generate()
 		fTerrainX = -TERRAINSIZE / 2;
 		for (int x = 0; x < TERRAIN_X; x++)
 		{
+			// generate height for the vertex
+			float height = generateHeight(glm::vec3(fTerrainX, Y_POSITION, fTerrainZ), 0, 2.0, 7);
+
+			// store vertex
 			vertices.push_back(fTerrainX);
-			vertices.push_back(Y_POSITION);
+			vertices.push_back(height);
 			vertices.push_back(fTerrainZ);
 
 			fTerrainX += (TERRAINSIZE / (TERRAIN_X - 1));
@@ -71,3 +75,62 @@ void TerrainMesh::generate()
 		}
 	}
 }
+
+/*
+*  Perlin noise basis function
+*/
+float Basis(glm::vec3 point)
+{
+	float r = rand() / (float)RAND_MAX;
+	return r * 3.0f;
+}
+
+/*
+* Procedural fBm evaluated at "point"; returns value stored in "value".
+*
+* Parameters:
+* "H" is the fractal increment
+* "lacunarity" is the gap between successive frequencies
+* "octaves" is the number of frequencies in the fBm
+*/
+float TerrainMesh::generateHeight(glm::vec3 point, float H, float lacunarity, int octaves)
+{
+	float value, frequency, remainder;
+	int i;
+	bool first = true;
+	const int MAX_OCTAVES = 5;
+	float exponent_array[MAX_OCTAVES];
+
+	/* precompute and store spectral weights */
+	if (first) 
+	{
+		frequency = 1.0;
+		for (i = 0; i<MAX_OCTAVES; i++)
+		{
+			/* compute weight for each frequency */
+			exponent_array[i] = pow(frequency, -H);
+			frequency *= lacunarity;
+		}
+		first = false;
+	}
+
+	value = 0.0;
+	/* inner loop of spectral construction */
+	for (i = 0; i<octaves; i++) 
+	{
+		float noise = Basis(point);
+		value += noise;// *exponent_array[i];
+		point.x *= lacunarity;
+		point.y *= lacunarity;
+		point.z *= lacunarity;
+	} 
+	
+	remainder = octaves - (int)octaves;
+	if (remainder) /* add in "octaves" remainder */
+	{
+		/* "i" and spatial freq. are preset in loop above */
+		value += remainder * Basis(point) * exponent_array[i];
+	}
+	return(value);
+}
+
